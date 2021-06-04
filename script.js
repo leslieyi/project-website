0e683877f3cdbb353e796cd29600953e9b3d4ae1
Yearim Leslie Yi <leslieyi411@gmail.com>
2021-06-04 17:55:13 -0400

    little bit of this and that

---
+++ script.js
@@ -0,0 +1,13 @@
+function switchPage(id) {
+    console.log("switchPage", id)
+    const pages = document.getElementsByClassName("page")
+    for (const page of pages) {
+        page.style.visibility = "hidden"
+    }
+    const element = document.getElementById(id)
+    element.style.visibility = "visible"
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    switchPage("main")
+});
